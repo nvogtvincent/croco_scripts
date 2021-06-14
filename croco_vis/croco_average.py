3463abc05a0af8e92446acb10457c971e428a8c3
Noam <noam.vogt-vincent@st-annes.ox.ac.uk>
2021-06-14 18:58:28 +0100

    Reduced memory use of croco_average

--- croco_vis/croco_average.py
+++ croco_vis/croco_average.py
@@ -1,101 +1,64 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Script to calculate a moving gaussian average from CROCO output data
+Script to calculate a moving average from CROCO output data for very large
+datasets
 @author: noam
 """
 
 from netCDF4 import Dataset
+from scipy.ndimage import convolve1d
 import numpy as np
 import sys
 import os
+from progress.bar import Bar
 
 ##############################################################################
 # Parameters #################################################################
 ##############################################################################
 
-climvar = 'v'
 out_freq = 3    # Hours
 window   = 24   # Hours
+
+in_fh = '1993_vis.nc'
+out_fh = '1993_vis_filtered.nc'
+
+variables = ['temp_surf']
 
 ##############################################################################
 # File locations #############################################################
 ##############################################################################
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
-
-# Generator for file names
-namelist = []
-for i in range(2, 10):
-    namelist.append('Y' + str(i+1) + '_30M.nc')
-
-n_files = len(namelist)
-
-# Check for climvar
-if climvar in ['temp', 'salt', 'u', 'v']:
-    climvar = climvar + '_surf'
-elif climvar != 'zeta':
-    raise ValueError('Unknown variable')
-
-
-# List of month lengths
-day_per_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-day_cumsum = np.cumsum(day_per_month)
-
-if climvar in ['u_surf', 'v_surf']:
-    files_per_day = 48
-else:
-    files_per_day = 1
-
-# Output
-output_file = this_dir + '/' + 'croco_climatology_' + climvar + '.nc'
+in_fh = this_dir + '/' + in_fh
+out_fh = this_dir + '/' + out_fh
 
 ##############################################################################
-# Calculate climatology ######################################################
+# Generate output file #######################################################
 ##############################################################################
 
-for i in range(n_files):
-    input_file = this_dir + '/' + namelist[i]
-    print(input_file)
+# Firstly load to obtain dimensions
+with Dataset(in_fh, mode='r') as nc:
+    print('Loading files...')
+    time = np.shape(nc.variables['time_counter_bounds'][:, 0])[0]
+    lon = nc.variables['lon_rho'][:]
+    lat = nc.variables['lat_rho'][:]
 
-    with Dataset(input_file, mode='r') as nc:
-        if i == 0:
-            # Set up the output array if this is the first file
-            if climvar in ['temp_surf', 'salt_surf', 'zeta']:
-                lon = nc.variables['lon_rho'][:]
-                lat = nc.variables['lat_rho'][:]
-            elif climvar in ['u_surf']:
-                lon = nc.variables['nav_lon_u'][:]
-                lat = nc.variables['nav_lat_u'][:]
-            else:
-                lon = nc.variables['nav_lon_v'][:]
-                lat = nc.variables['nav_lat_v'][:]
-
-            climatology = np.zeros((12, np.shape(lon)[0], np.shape(lon)[1]))
-
-        for t in range(365*files_per_day):
-            month = np.searchsorted(day_cumsum, t/files_per_day, side='right')
-            climatology[month, :, :] += (
-                (1/(files_per_day*day_per_month[month]*n_files)) *
-                nc.variables[climvar][t, :, :])
-
-
-# Create the climatology to a new file
-with Dataset(output_file, mode='w') as nc:
-    print('Writing fields to output netcdf...')
+# Now generate the output file
+with Dataset(out_fh, mode='w') as nc:
+    print('Generating output netcdf...')
     # Create the dimensions
-    nc.createDimension('time', 12)
+    nc.createDimension('time', time)
     nc.createDimension('lon', np.shape(lon)[1])
     nc.createDimension('lat', np.shape(lon)[0])
 
     # Create the time axis
     nc.createVariable('time', 'f8', ('time'), zlib=True)
-    nc.variables['time'].long_name = 'time'
-    nc.variables['time'].units = 'months'
+    nc.variables['time'].long_name = 'seconds_since_start'
+    nc.variables['time'].units = 'seconds'
     nc.variables['time'].field = 'time, scalar, series'
-    nc.variables['time'].standard_name = 'climatology_time'
-    nc.variables['time'].axis = 'T'
-    nc.variables['time'][:] = np.linspace(1, 12, num=12, dtype=np.int8)
+    nc.variables['time'].standard_name = 'seconds_since_start'
+    nc.variables['time'][:] = time
 
     nc.createVariable('longitude', 'f8', ('lat', 'lon'), zlib=True)
     nc.variables['longitude'].long_name = 'longitude'
@@ -112,45 +75,57 @@ with Dataset(output_file, mode='w') as nc:
     nc.variables['latitude'][:] = lat
 
     # Create the other variables
-    if climvar == 'temp_surf':
+    if 'temp_surf' in variables:
         nc.createVariable('sst', 'f8', ('time', 'lat', 'lon'))
-        nc.variables['sst'].long_name = 'sea_surface_temperature'
+        nc.variables['sst'].long_name = 'filtered_sea_surface_temperature'
         nc.variables['sst'].units = 'Celsius'
         nc.variables['sst'].field = 'temperature, scalar, series'
         nc.variables['sst'].standard_name = 'sst'
         nc.variables['sst'].coordinates = 'lat lon'
-        nc.variables['sst'][:] = climatology
-    elif climvar == 'salt_surf':
+    if 'salt_surf' in variables:
         nc.createVariable('sss', 'f8', ('time', 'lat', 'lon'))
-        nc.variables['sss'].long_name = 'sea_surface_salinity'
+        nc.variables['sss'].long_name = 'filtered_sea_surface_salinity'
         nc.variables['sss'].units = 'PSU'
         nc.variables['sss'].field = 'salinity, scalar, series'
         nc.variables['sss'].standard_name = 'sss'
         nc.variables['sss'].coordinates = 'lat lon'
-        nc.variables['sss'][:] = climatology
-    elif climvar == 'zeta':
+    if 'zeta' in variables:
         nc.createVariable('ssh', 'f8', ('time', 'lat', 'lon'))
-        nc.variables['ssh'].long_name = 'sea_surface_height'
+        nc.variables['ssh'].long_name = 'filtered_sea_surface_height'
         nc.variables['ssh'].units = 'meter'
         nc.variables['ssh'].field = 'free-surface, scalar, series'
         nc.variables['ssh'].standard_name = 'ssh'
         nc.variables['ssh'].coordinates = 'lat lon'
-        nc.variables['ssh'][:] = climatology
-    elif climvar == 'u_surf':
-        nc.createVariable('u', 'f8', ('time', 'lat', 'lon'))
-        nc.variables['u'].long_name = 'u-momentum component'
-        nc.variables['u'].units = 'meter second-1'
-        nc.variables['u'].field = 'u-velocity, scalar, series'
-        nc.variables['u'].standard_name = 'sea_water_x_velocity_at_u_location'
-        nc.variables['u'].coordinates = 'lat lon'
-        nc.variables['u'][:] = climatology
-    elif climvar == 'v_surf':
-        nc.createVariable('v', 'f8', ('time', 'lat', 'lon'))
-        nc.variables['v'].long_name = 'v-momentum component'
-        nc.variables['v'].units = 'meter second-1'
-        nc.variables['v'].field = 'v-velocity, scalar, series'
-        nc.variables['v'].standard_name = 'sea_water_y_velocity_at_v_location'
-        nc.variables['v'].coordinates = 'lat lon'
-        nc.variables['v'][:] = climatology
 
-    print('Completed!')
+# Set up the filter
+window_len = int(window/out_freq)
+croco_filter = (1/window_len)*np.ones(window_len)
+
+# Then loop through the columns
+bar = Bar('Filtering data...', max=np.shape(lon)[0])
+for i in range(np.shape(lon)[0]):
+    with Dataset(in_fh, mode='r') as nc:
+        if 'temp_surf' in variables:
+            temp = nc.variables['temp_surf'][:, i, :]
+            temp = convolve1d(temp, croco_filter, axis=0)
+        if 'salt_surf' in variables:
+            salt = nc.variables['salt_surf'][:, i, :]
+            salt = convolve1d(salt, croco_filter, axis=0)
+        if 'zeta' in variables:
+            zeta = nc.variables['zeta'][:, i, :]
+            zeta = convolve1d(zeta, croco_filter, axis=0)
+
+    with Dataset(out_fh, mode='r+') as nc:
+        if 'temp_surf' in variables:
+            nc.variables['sst'][:, i, :] = temp
+        if 'salt_surf' in variables:
+            nc.variables['sss'][:, i, :] = salt
+        if 'zeta' in variables:
+            nc.variables['ssh'][:, i, :] = zeta
+
+    bar.next()
+bar.finish()
+
+print('Processing complete!')
+
+
