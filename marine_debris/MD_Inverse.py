3171c17de6c46a360d81ca612f8269be4f041f8c
Noam <noam.vogt-vincent@st-annes.ox.ac.uk>
2021-10-06 15:28:45 +0100

    Proper testing mode implemented

--- marine_debris/MD_Inverse.py
+++ marine_debris/MD_Inverse.py
@@ -6,9 +6,11 @@ CMEMS GLORYS12V1 reanalysis (current and Stokes drift)
 @author: Noam Vogt-Vincent
 """
 
+import os
 import MD_Methods as mdm
 import numpy as np
-import os
+import matplotlib.pyplot as plt
+import cmocean.cm as cm
 from parcels import (Field, FieldSet, ParticleSet, JITParticle, AdvectionRK4,
                      ErrorCode, Geographic, GeographicPolar, Variable)
 from netCDF4 import Dataset, num2date
@@ -40,19 +42,20 @@ param = {# Release timing
 
          # Runtime parameters
          'Yend'              : 2019,          # Last year of simulation
-         'Mend'              : 1   ,          # Last month
-         'Dend'              : 2   ,          # Last day (00:00, start)
-         'dt_RK4'            : timedelta(minutes=-15),  # RK4 time-step
+         'Mend'              : 11  ,          # Last month
+         'Dend'              : 25   ,          # Last day (00:00, start)
+         'dt_RK4'            : timedelta(minutes=-30),  # RK4 time-step
 
          # Output parameters
-         'dt_out'            : timedelta(hours=6),     # Output frequency
-         'fn_out'            : 'test.nc',                 # Output filename
+         'dt_out'            : timedelta(hours=1),     # Output frequency
+         'fn_out'            : 'test.nc',              # Output filename
 
          # Other parameters
          'update'            : True,                   # Update grid files
          'plastic'           : True,                   # Write plastic data
          'p_param'           : {'l'  : 50.,            # Plastic length scale
-                                'cr' : 0.15}}          # Fraction entering sea
+                                'cr' : 0.15},          # Fraction entering sea
+         'test'              : True}                   # Activate test mode
 
 # DIRECTORIES
 dirs = {'script': os.path.dirname(os.path.realpath(__file__)),
@@ -106,7 +109,31 @@ grid = mdm.gridgen(fh, dirs, param, plastic=True)
 particles = {'time_array' : mdm.release_time(param, mode=param['mode'])}
 
 # Calculate the locations for particle releases
-particles['loc_array'] = mdm.release_loc(param, fh)
+if not param['test']:
+    particles['loc_array'] = mdm.release_loc(param, fh)
+else:
+    # For testing mode, initialise a small number of particles in a small grid
+    particles['loc_array'] = {}
+    particles['loc_array']['lon0'] = 49.2
+    particles['loc_array']['lon1'] = 49.2
+    particles['loc_array']['lat0'] = -12.4
+    particles['loc_array']['lat1'] = -11.8
+
+    particles['loc_array']['ll'] = [np.linspace(particles['loc_array']['lon0'],
+                                                particles['loc_array']['lon1'],
+                                                num=10),
+                                    np.linspace(particles['loc_array']['lat0'],
+                                                particles['loc_array']['lat1'],
+                                                num=10),]
+
+
+    particles['loc_array']['lon'] = particles['loc_array']['ll'][0].flatten()
+    particles['loc_array']['lat'] = particles['loc_array']['ll'][1].flatten()
+
+    particles['loc_array']['iso'] = np.zeros_like(particles['loc_array']['lon'])
+    particles['loc_array']['id'] = np.zeros_like(particles['loc_array']['lon'])
+
+    print()
 
 # Calculate a final array of release positions, IDs, and times
 particles['pos'] = mdm.add_times(particles)
@@ -134,7 +161,7 @@ dimensions = {'U': {'lon': 'longitude', 'lat': 'latitude', 'time': 'time'},
               'V': {'lon': 'longitude', 'lat': 'latitude', 'time': 'time'}}
 
 fieldset_ocean = FieldSet.from_netcdf(filenames, variables, dimensions,
-                                      chunksize=cs_OCEAN)
+                                      chunksize=False)
 
 # WAVE (STOKES FROM WAVERYS W/ GLORYS12V1)
 filenames = fh['wave']
@@ -146,7 +173,7 @@ dimensions = {'U': {'lon': 'longitude', 'lat': 'latitude', 'time': 'time'},
               'V': {'lon': 'longitude', 'lat': 'latitude', 'time': 'time'}}
 
 fieldset_wave = FieldSet.from_netcdf(filenames, variables, dimensions,
-                                      chunksize=cs_WAVE)
+                                      chunksize=False)
 
 fieldset = FieldSet(U=fieldset_ocean.U+fieldset_wave.U,
                     V=fieldset_ocean.V+fieldset_wave.V)
@@ -315,3 +342,126 @@ pset.execute(kernels,
              output_file=traj)
 
 traj.export()
+
+##############################################################################
+# PLOT TEST CASE                                                             #
+##############################################################################
+
+if param['test']:
+    # Set display region
+    (lon_min, lon_max, lat_min, lat_max) = (48.7, 49.7, -12.5, -11.5)
+
+    # Import grids
+    with Dataset(fh['grid'], mode='r') as nc:
+        lon_psi = grid['lon_psi']
+        lat_psi = grid['lat_psi']
+        lon_rho = grid['lon_rho']
+        lat_rho = grid['lat_rho']
+
+        lsm_psi = grid['lsm_psi']
+        lsm_rho = grid['lsm_rho']
+        coast_psi = grid['coast_psi']
+
+        cnormx_rho = grid['cnormx_rho']
+        cnormy_rho = grid['cnormy_rho']
+
+        cdist_rho = grid['cdist_rho']
+
+    jmin_psi = np.searchsorted(lon_psi, lon_min) - 1
+    if jmin_psi < 0:
+        jmin_psi = 0
+    jmin_rho = jmin_psi
+    jmax_psi = np.searchsorted(lon_psi, lon_max)
+    jmax_rho = jmax_psi + 1
+
+    imin_psi = np.searchsorted(lat_psi, lat_min) - 1
+    imin_rho = imin_psi
+    imax_psi = np.searchsorted(lat_psi, lat_max)
+    imax_rho = imax_psi + 1
+
+    disp_lon_rho = lon_rho[jmin_rho:jmax_rho]
+    disp_lat_rho = lat_rho[imin_rho:imax_rho]
+
+    disp_lon_psi = lon_psi[jmin_psi:jmax_psi]
+    disp_lat_psi = lat_psi[imin_psi:imax_psi]
+
+    disp_lsm_psi   = lsm_psi[imin_psi:imax_psi, jmin_psi:jmax_psi]
+    disp_lsm_rho   = lsm_rho[imin_rho:imax_rho, jmin_rho:jmax_rho]
+
+    disp_coast_psi = coast_psi[imin_psi:imax_psi, jmin_psi:jmax_psi]
+    disp_cdist_rho = cdist_rho[imin_rho:imax_rho, jmin_rho:jmax_rho]
+
+    with Dataset(fh['ocean'][-1], mode='r') as nc:
+        disp_u_rho   = nc.variables['uo'][0, 0,
+                                          imin_rho:imax_rho,
+                                          jmin_rho:jmax_rho]
+
+        disp_v_rho   = nc.variables['vo'][0, 0,
+                                          imin_rho:imax_rho,
+                                          jmin_rho:jmax_rho]
+
+    cnormx   = cnormx_rho[imin_rho:imax_rho,jmin_rho:jmax_rho]
+    cnormy   = cnormy_rho[imin_rho:imax_rho,jmin_rho:jmax_rho]
+
+    # Plot the map
+    f, ax = plt.subplots(1, 1, figsize=(10, 10))
+
+    ax.set_xlim(lon_min, lon_max)
+    ax.set_ylim(lat_min, lat_max)
+
+    # Plot the rho grid
+    for i in range(len(disp_lat_rho)):
+        ax.plot([lon_min, lon_max], [disp_lat_rho[i], disp_lat_rho[i]],
+                'k--', linewidth=0.5)
+
+    for j in range(len(disp_lon_rho)):
+        ax.plot([disp_lon_rho[j], disp_lon_rho[j]], [lat_min, lat_max],
+                'k--', linewidth=0.5)
+
+    # Plot the lsm_psi mask
+    disp_lon_rho_, disp_lat_rho_ = np.meshgrid(disp_lon_rho, disp_lat_rho)
+    disp_lon_psi_, disp_lat_psi_ = np.meshgrid(disp_lon_psi, disp_lat_psi)
+
+    ax.pcolormesh(disp_lon_rho, disp_lat_rho, disp_lsm_psi, cmap=cm.topo,
+                  vmin=-0.5, vmax=1.5)
+
+    # Plot the coast_psi mask
+    ax.pcolormesh(disp_lon_rho, disp_lat_rho,
+                  np.ma.masked_values(disp_coast_psi, 0), cmap=cm.topo,
+                  vmin=0, vmax=3)
+
+    # Plot the lsm_rho nodes
+    ax.scatter(disp_lon_rho_, disp_lat_rho_, c=disp_lsm_rho, s=10, marker='o',
+                cmap=cm.gray_r)
+
+    # # Plot the  lsm_psi nodes
+    # ax.scatter(disp_lon_psi_, disp_lat_psi_, s=20, marker='+', c=disp_lsm_psi,
+    #             cmap=cm.gray_r, linewidth=0.3)
+
+    # Plot the velocity field and BCs
+    ax.quiver(disp_lon_rho, disp_lat_rho, disp_u_rho, disp_v_rho)
+    ax.quiver(disp_lon_rho, disp_lat_rho, cnormx, cnormy, units='inches', scale=3, color='w')
+
+    # Load the trajectories
+    with Dataset(fh['traj'], mode='r') as nc:
+        plat  = nc.variables['lat'][:]
+        plon  = nc.variables['lon'][:]
+        # pstate = nc.variables['lsm'][:]
+
+    pnum = np.shape(plat)[0]
+    pt   = np.shape(plat)[1]
+
+    for particle in range(pnum):
+        # if plon[particle, 0] == plon[particle, 1]:
+        #     ax.scatter(plon[particle, 0], plat[particle, 0], c='r', s=15, marker='o')
+        # else:
+        #     ax.scatter(plon[particle, 0], plat[particle, 0], c='b', s=15, marker='o')
+        ax.plot(plon[particle, :], plat[particle, :], 'w-', linewidth=0.5)
+        # ax.scatter(plon[particle, :], plat[particle, :],
+        #            c = pstate[particle, :],
+        #            cmap = cm.gray_r,
+        #            s = 10,
+        #            marker = 's')
+
+    # Save
+    plt.savefig(dirs['script'] + '/' + 'test_fig.png', dpi=300)
