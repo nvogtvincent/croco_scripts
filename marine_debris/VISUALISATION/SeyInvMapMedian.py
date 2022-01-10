fb9416a9ff2d96e79a97f6852e04ca5a673f8674
Noam <noam.vogt-vincent@st-annes.ox.ac.uk>
2022-01-10 17:10:41 +0000

    Updated for ot/ct stats

--- marine_debris/VISUALISATION/SeyInvMapMedian.py
+++ marine_debris/VISUALISATION/SeyInvMapMedian.py
@@ -21,6 +21,7 @@ import cmasher as cmr
 import numpy as np
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+import matplotlib.ticker as mticker
 from netCDF4 import Dataset
 from numba import njit
 from psutil import virtual_memory
@@ -32,19 +33,15 @@ from glob import glob
 ###############################################################################
 
 # PARAMETERS
-param = {'grid_res': 1.0,                          # Grid resolution in degrees
+param = {'grid_res': 1,                          # Grid resolution in degrees
          'lon_range': [-180, 180],                 # Longitude range for output
          'lat_range': [-90, 90],                   # Latitude range for output
 
-         'lb': 1/(3600*24*60),
-         'ls': 1/(3600*24*3650),
+         'lb': 60,                                 # Days
+         'ls': 3650,                               # Days
 
          'days_per_obs': 5,                        # Days represented by each obs
-         'percentile': 50,
-
-         'out_name_1': 'loss_map.png',
-         'out_name_2': 'dens_map_annual_mean.png',
-         'out_name_3': 'dens_map_seasons'}     # Output figure name
+         'percentile': 90,}
 
 # DIRECTORIES
 dirs = {'script': os.path.dirname(os.path.realpath(__file__)) + '/../',
@@ -52,13 +49,13 @@ dirs = {'script': os.path.dirname(os.path.realpath(__file__)) + '/../',
         'fig': os.path.dirname(os.path.realpath(__file__)) + '/../FIGURES/'}
 
 # FILE HANDLES
-fh = {'traj': sorted(glob(dirs['traj'] + '*.nc')),
-      'fig': [dirs['fig'] + param['out_name_1'],
-              dirs['fig'] + param['out_name_2'],
-              dirs['fig'] + param['out_name_3']]}
+fh = {'traj': sorted(glob(dirs['traj'] + '*SeyBwd.nc')),
+      'fig': dirs['fig'] + 'b' + str(param['lb']) +
+      '_s' + str(param['ls']) + '_p' + str(param['percentile'])}
 
 red_mem = 100 # (workaround for HPC)
-
+param['lb'] = 1/(3600*24*param['lb'])
+param['ls'] = 1/(3600*24*param['ls'])
 ###############################################################################
 # Routines ####################################################################
 ###############################################################################
@@ -110,14 +107,18 @@ def get_traj(fh, fh_num, start, end, **kwargs):
             tb_out.compressed()]
 
 
-def calc_loss(lon_bnd, lat_bnd, mass_bnd, param, fh):
+def calc_loss(lon_bnd, lat_bnd, mass_bnd, ot_bnd, ct_bnd, param, fh):
     # Calculate available memory
     avail_mem = virtual_memory()[4]
 
     # Generate the empty grids
-    # tot_grid = np.zeros((len(lat_bnd)-1, len(lon_bnd)-1, 120), dtype=np.float32)
-    hist_grid = np.zeros((len(lat_bnd)-1, len(lon_bnd)-1, 100), dtype=np.int32)
-    grid = np.zeros((len(lat_bnd)-1, len(lon_bnd)-1), dtype=np.float32)
+    hist_grid_massloss = np.zeros((len(lat_bnd)-1, len(lon_bnd)-1, len(mass_bnd)-1), dtype=np.int32)
+    hist_grid_ot = np.zeros((len(lat_bnd)-1, len(lon_bnd)-1, len(ot_bnd)-1), dtype=np.int32)
+    hist_grid_ct = np.zeros((len(lat_bnd)-1, len(lon_bnd)-1, len(ct_bnd)-1), dtype=np.int32)
+
+    grid_massloss = np.zeros((len(lat_bnd)-1, len(lon_bnd)-1), dtype=np.float32)
+    grid_ot = np.zeros((len(lat_bnd)-1, len(lon_bnd)-1), dtype=np.float32)
+    grid_ct = np.zeros((len(lat_bnd)-1, len(lon_bnd)-1), dtype=np.float32)
 
     # Loop over trajectory files
     for fh_num, traj_fh in enumerate(fh['traj']):
@@ -128,7 +129,7 @@ def calc_loss(lon_bnd, lat_bnd, mass_bnd, param, fh):
             n_ch = int(np.ceil(ntraj/ntraj_ch))
 
         # Loop over chunks
-        for chunk in range(n_ch): # Change to n_ch
+        for chunk in range(n_ch):
             # Load data
             data = get_traj(fh, fh_num,
                             ntraj_ch*chunk,
@@ -136,67 +137,37 @@ def calc_loss(lon_bnd, lat_bnd, mass_bnd, param, fh):
 
             # Calculate mass ratio at each point
             mass = np.exp((-param['lb']*data[3])+(-param['ls']*data[2]))
-            hist_grid += np.histogramdd(np.array([data[1], data[0], mass]).T,
-                                        (lat_bnd, lon_bnd, mass_bnd))[0].astype(np.int32)
+
+            ot = data[2]/(3600*24*365.25) # Days
+            ct = data[3]/(3600*24*365.25) # Days
+
+            hist_grid_massloss += np.histogramdd(np.array([data[1], data[0], mass]).T,
+                                                 (lat_bnd, lon_bnd, mass_bnd))[0].astype(np.int32)
+            hist_grid_ot += np.histogramdd(np.array([data[1], data[0], ot]).T,
+                                           (lat_bnd, lon_bnd, ot_bnd))[0].astype(np.int32)
+            hist_grid_ct += np.histogramdd(np.array([data[1], data[0], ct]).T,
+                                           (lat_bnd, lon_bnd, ct_bnd))[0].astype(np.int32)
 
     # Finally, calculate the specified percentile
-    def find_percentile(lon_bnd, lat_bnd, mass_bnd, grid_in, percentile):
-        mass_mp = 0.5*(mass_bnd[1:] + mass_bnd[:-1])
+    def find_percentile(lon_bnd, lat_bnd, val_bnd, grid_in, percentile):
+        val_mp = 0.5*(val_bnd[1:] + val_bnd[:-1])
         grid = np.ones(np.shape(grid_in)[:2])
 
         for lat_idx in range(len(lat_bnd)-1):
             for lon_idx in range(len(lon_bnd)-1):
                 hist = grid_in[lat_idx, lon_idx, :]
                 if np.sum(hist) > 0:
-                    grid[lat_idx, lon_idx] = np.percentile(np.repeat(mass_mp, hist), percentile)
+                    grid[lat_idx, lon_idx] = np.percentile(np.repeat(val_mp, hist), percentile)
                 else:
                     grid[lat_idx, lon_idx] = -1
 
-        return grid
+        return np.ma.masked_where(grid < 0, grid)
 
-    grid = find_percentile(lon_bnd, lat_bnd, mass_bnd, hist_grid, param['percentile'])
-    grid = np.ma.masked_where(grid < 0, grid)
+    grid_massloss = find_percentile(lon_bnd, lat_bnd, mass_bnd, hist_grid_massloss, param['percentile'])
+    grid_ot = find_percentile(lon_bnd, lat_bnd, ot_bnd, hist_grid_ot, 100-param['percentile'])
+    grid_ct = find_percentile(lon_bnd, lat_bnd, ct_bnd, hist_grid_ct, 100-param['percentile'])
 
-    return grid
-
-
-def calc_dens(lon_bnd, lat_bnd, init_times, param, fh):
-    # Calculate available memory
-    avail_mem = virtual_memory()[4]
-
-    # Generate the empty grids
-    num_grid = np.zeros((len(lat_bnd)-1, len(lon_bnd)-1), dtype=np.int32)
-
-    # Calculate number of chunks
-    with Dataset(fh['traj'][0], mode='r') as nc:
-        ntraj, ntime = np.shape(nc.variables['lon'])
-        ntraj_ch = int(avail_mem/(ntime*8*red_mem))
-        n_ch = int(np.ceil(ntraj/ntraj_ch))
-
-    ntraj_tot = 0
-
-    # Extract rows with correct time
-    for fh_num, traj_fh in enumerate(fh['traj']):
-        for chunk in range(n_ch): # Change to n_ch
-            # Load data
-            data = get_traj(fh, fh_num,
-                            ntraj_ch*chunk,
-                            ntraj_ch*(chunk+1),
-                            initial=init_times)[:3]
-
-            # Grid
-            num_grid += np.histogram2d(data[2], data[1],
-                                       (lat_bnd, lon_bnd))[0].astype(np.int32)*param['days_per_obs']
-
-            # Keep track of number of particles
-            ntraj_tot += data[0]
-
-    # If ntraj_tot == 0, avoid NaN
-    if ntraj_tot == 0:
-        print('Warning: total trajectories is 0')
-        ntraj_tot = 1
-
-    return num_grid/ntraj_tot
+    return grid_massloss, grid_ot, grid_ct
 
 
 ###############################################################################
@@ -216,153 +187,143 @@ if __name__ == '__main__':
                                param['lat_range'][0])/param['grid_res'])+1)
 
     mass_bnd = np.linspace(0, 1, num=101)
+    ot_bnd = np.linspace(0, 12, num=145)
+    ct_bnd = np.linspace(0, 1, num=101)
+    ot_bnd[-1] = 30
+    ct_bnd[-1] = 30
 
     print('Gridding trajectories...')
-
-    # Get initial times for all months
-    # init_times = get_init_times(param, fh)
-    # dens_month_grid = []
-    # for month in range(12):
-    #     # Grid locations for this month
-    #     dens_month_grid.append(calc_dens(lon_bnd, lat_bnd, init_times[:, month], param, fh))
-
-    # dens_month_grid = np.array(dens_month_grid)
-
-    # # Calculate monthly/annual stats
-    # annual_mean = np.mean(dens_month_grid, axis=0)
-    # seasonal_means = [np.mean(dens_month_grid[0:3, :, :], axis=0),
-    #                   np.mean(dens_month_grid[3:6, :, :], axis=0),
-    #                   np.mean(dens_month_grid[6:9, :, :], axis=0),
-    #                   np.mean(dens_month_grid[9:12, :, :], axis=0)]
 
     ###########################################################################
     # Plotting ################################################################
     ###########################################################################
 
-    # f0, a0 = plt.subplots(1, 1, figsize=(20, 10),
-    #                       subplot_kw={'projection': ccrs.PlateCarree()})
-    # f0.subplots_adjust(hspace=0, wspace=0, top=0.925, left=0.1)
-    # a0.set_ylim([-70, 70])
-
-    # axpos = a0.get_position()
-    # pos_x = axpos.x0+axpos.width + 0.41
-    # pos_y = axpos.y0
-    # cax_width = 0.015
-    # cax_height = axpos.height
-
-    # pos_cax = f0.add_axes([pos_x, pos_y, cax_width, cax_height])
-
-    # # Set up the colormap (from https://stackoverflow.com/questions/14777066/matplotlib-discrete-colorbar)
-    # cmap = cmr.ocean
-    # dens = a0.pcolormesh(lon_bnd, lat_bnd, annual_mean, cmap=cmap,
-    #                      norm=colors.LogNorm(vmin=1e-5, vmax=1e0))
-
-    # # Add cartographic features
-    # gl = a0.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
-    #                   linewidth=0.5, color='black', linestyle='-', zorder=11)
-    # gl.xlabels_top = False
-    # gl.ylabels_right = False
-
-    # land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m',
-    #                                         edgecolor='face',
-    #                                         facecolor='black')
-    # a0.add_feature(land_10m)
-
-    # cb = plt.colorbar(dens, cax=pos_cax)
-    # cb.set_label('Mean time spent in cell (days)', size=12)
-    # a0.set_aspect('auto', adjustable=None)
-    # a0.margins(x=-0.01, y=-0.01)
-    # plt.savefig(fh['fig'][1], dpi=300)
-
-    # # Anomalies
-    # for i in range(4):
-    #     f1, a1 = plt.subplots(1, 1, figsize=(20, 10),
-    #                           subplot_kw={'projection': ccrs.PlateCarree()})
-    #     f1.subplots_adjust(hspace=0, wspace=0, top=0.925, left=0.1)
-
-    #     axpos = a1.get_position()
-    #     pos_x = axpos.x0+axpos.width + 0.41
-    #     pos_y = axpos.y0
-    #     cax_width = 0.015
-    #     cax_height = axpos.height
-
-    #     pos_cax = f1.add_axes([pos_x, pos_y, cax_width, cax_height])
-
-    #     # Set up the colormap (from https://stackoverflow.com/questions/14777066/matplotlib-discrete-colorbar)
-    #     cmap = cmr.fusion
-    #     dens = a1.pcolormesh(lon_bnd, lat_bnd, seasonal_means[i]/annual_mean, cmap=cmap,
-    #                          norm=colors.LogNorm(vmin=1/5, vmax=5))
-
-    #     # Add cartographic features
-    #     gl = a1.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
-    #                       linewidth=0.5, color='black', linestyle='-', zorder=11)
-    #     gl.xlabels_top = False
-    #     gl.ylabels_right = False
-
-    #     land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m',
-    #                                             edgecolor='face',
-    #                                             facecolor='black')
-    #     a1.add_feature(land_10m)
-
-    #     cb = plt.colorbar(dens, cax=pos_cax)
-    #     season = ['winter', 'spring', 'summer', 'autumn']
-    #     cb.set_label('Mean time spent in cell (days) ratio between ' + season[i] + ' and annual mean', size=12)
-    #     a1.set_aspect('auto', adjustable=None)
-    #     a1.margins(x=-0.01, y=-0.01)
-    #     plt.savefig(fh['fig'][2] + '_' + season[i] + '.png' , dpi=300)
-    #     plt.close()
-
-    # print()
-    # print('check')
-
-
     # Calculate losses from beaching and sinking
-    loss_grid = calc_loss(lon_bnd, lat_bnd, mass_bnd, param, fh)
+    grids = calc_loss(lon_bnd, lat_bnd, mass_bnd, ot_bnd, ct_bnd, param, fh)
 
     print('Calculation complete')
 
-    # Plotting
-    f2, a2 = plt.subplots(1, 1, figsize=(20, 10),
-                         subplot_kw={'projection': ccrs.PlateCarree()})
 
-    f2.subplots_adjust(hspace=0, wspace=0, top=0.925, left=0.1)
-    a2.set_ylim([-70, 70])
+    # Plotting loss
+    f, ax = plt.subplots(1, 1, figsize=(19, 10),
+                         subplot_kw={'projection': ccrs.Robinson(central_longitude=60)})
+
+    f.subplots_adjust(hspace=0, wspace=0, top=0.925, left=0.1)
+    ax.set_global()
+    ax.set_aspect(1)
 
     # Set up the colorbar
-    axpos = a2.get_position()
-    pos_x = axpos.x0+axpos.width + 0.41
-    pos_y = axpos.y0
-    cax_width = 0.015
-    cax_height = axpos.height
-
-    pos_cax = f2.add_axes([pos_x, pos_y, cax_width, cax_height])
+    pos_cax = f.add_axes([ax.get_position().x1+0.01,ax.get_position().y0-0.025,0.015,ax.get_position().height+0.05])
 
     # Plot the colormesh
     cmap = cmr.fall_r
-    hist = a2.pcolormesh(lon_bnd, lat_bnd, loss_grid, cmap=cmap, vmin=0, vmax=1)
+    # cmaplist = [cmap(i) for i in range(cmap.N)]
+    # cmap = colors.LinearSegmentedColormap.from_list('dense_sq', cmaplist, cmap.N)
+    # cmap_bounds = np.linspace(0, 1, 11)
+    # cmap_norm = colors.BoundaryNorm(cmap_bounds, cmap.N)
+
+    hist = ax.pcolormesh(lon_bnd, lat_bnd, grids[0], cmap=cmap, vmin=0, vmax=1,
+                         transform=ccrs.PlateCarree())
 
     # Add cartographic features
-    gl = a2.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
-                      linewidth=0.5, color='black', linestyle='-', zorder=11)
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                      linewidth=0.5, color='black', linestyle='--', zorder=11)
+    gl.xlocator = mticker.FixedLocator(np.arange(-180, 240, 60))
+    gl.ylocator = mticker.FixedLocator(np.arange(-90, 120, 30))
     gl.xlabels_top = False
     gl.ylabels_right = False
 
     land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m',
                                             edgecolor='face',
-                                            facecolor='black')
-    a2.add_feature(land_10m)
-
-    # a0.add_feature(cfeature.LAND, facecolor='black', zorder=10)
+                                            facecolor='gray')
+    ax.add_feature(land_10m)
 
     cb = plt.colorbar(hist, cax=pos_cax)
     cb.set_label('Mass fraction reaching Seychelles', size=12)
-    a2.set_aspect('auto', adjustable=None)
-    a2.margins(x=-0.01, y=-0.01)
-    plt.savefig(fh['fig'][0], dpi=300)
+    ax.set_aspect('auto', adjustable=None)
+    ax.margins(x=-0.01, y=-0.01)
+    plt.savefig(fh['fig'] + '_loss.png', dpi=300)
+    plt.close()
 
 
+    # Plotting ocean time
+    f, ax = plt.subplots(1, 1, figsize=(19, 10),
+                         subplot_kw={'projection': ccrs.Robinson(central_longitude=60)})
 
+    f.subplots_adjust(hspace=0, wspace=0, top=0.925, left=0.1)
+    ax.set_global()
+    ax.set_aspect(1)
 
+    # Set up the colorbar
+    pos_cax = f.add_axes([ax.get_position().x1+0.01,ax.get_position().y0-0.025,0.015,ax.get_position().height+0.05])
 
+    # Plot the colormesh
+    cmap = cmr.ocean
+    cmaplist = [cmap(i) for i in range(cmap.N)]
+    cmap = colors.LinearSegmentedColormap.from_list('dense_sq', cmaplist, cmap.N)
+    cmap_bounds = np.linspace(0, 12, 13)
+    cmap_norm = colors.BoundaryNorm(cmap_bounds, cmap.N)
 
+    hist = ax.pcolormesh(lon_bnd, lat_bnd, grids[1], cmap=cmap, vmin=0, vmax=12,
+                         norm=cmap_norm, transform=ccrs.PlateCarree())
 
+    # Add cartographic features
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                      linewidth=0.5, color='black', linestyle='--', zorder=11)
+    gl.xlocator = mticker.FixedLocator(np.arange(-180, 240, 60))
+    gl.ylocator = mticker.FixedLocator(np.arange(-90, 120, 30))
+    gl.xlabels_top = False
+    gl.ylabels_right = False
+
+    land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m',
+                                            edgecolor='face',
+                                            facecolor='gray')
+    ax.add_feature(land_10m)
+
+    cb = plt.colorbar(hist, cax=pos_cax)
+    cb.set_label('Time at sea to Seychelles (years, 10th percentile)', size=12)
+    ax.set_aspect('auto', adjustable=None)
+    ax.margins(x=-0.01, y=-0.01)
+    plt.savefig(fh['fig'] + '_ot.png', dpi=300)
+    plt.close()
+
+    # Plotting ocean time
+    f, ax = plt.subplots(1, 1, figsize=(19, 10),
+                         subplot_kw={'projection': ccrs.Robinson(central_longitude=60)})
+
+    f.subplots_adjust(hspace=0, wspace=0, top=0.925, left=0.1)
+    ax.set_global()
+    ax.set_aspect(1)
+
+    # Set up the colorbar
+    pos_cax = f.add_axes([ax.get_position().x1+0.01,ax.get_position().y0-0.025,0.015,ax.get_position().height+0.05])
+
+    # Plot the colormesh
+    cmap = cmr.swamp_r
+    cmaplist = [cmap(i) for i in range(cmap.N)]
+    cmap = colors.LinearSegmentedColormap.from_list('dense_sq', cmaplist, cmap.N)
+    cmap_bounds = np.linspace(0, 12, 13)
+    cmap_norm = colors.BoundaryNorm(cmap_bounds, cmap.N)
+
+    hist = ax.pcolormesh(lon_bnd, lat_bnd, grids[2]*12, cmap=cmap, vmin=0, vmax=12,
+                         norm=cmap_norm, transform=ccrs.PlateCarree())
+
+    # Add cartographic features
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                      linewidth=0.5, color='black', linestyle='--', zorder=11)
+    gl.xlocator = mticker.FixedLocator(np.arange(-180, 240, 60))
+    gl.ylocator = mticker.FixedLocator(np.arange(-90, 120, 30))
+    gl.xlabels_top = False
+    gl.ylabels_right = False
+
+    land_10m = cfeature.NaturalEarthFeature('physical', 'land', '10m',
+                                            edgecolor='face',
+                                            facecolor='gray')
+    ax.add_feature(land_10m)
+
+    cb = plt.colorbar(hist, cax=pos_cax)
+    cb.set_label('Time along the coast enroute to Seychelles (months, 10th percentile)', size=12)
+    ax.set_aspect('auto', adjustable=None)
+    ax.margins(x=-0.01, y=-0.01)
+    plt.savefig(fh['fig'] + '_ct.png', dpi=300)
+    plt.close()
