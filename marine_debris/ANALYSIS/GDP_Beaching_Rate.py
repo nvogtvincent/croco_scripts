58cf086f46945839a1a50610dbe83e0aa9aad7bb
Noam <noam.vogt-vincent@st-annes.ox.ac.uk>
2022-02-02 19:37:01 +0000

    Semi-functional

--- marine_debris/ANALYSIS/GDP_Beaching_Rate.py
+++ marine_debris/ANALYSIS/GDP_Beaching_Rate.py
@@ -38,7 +38,7 @@ from shapely.geometry import Point
 # 4. Calculate F(beach) as a function of cumulative time within 1/12 of the coast
 
 # PARAMETERS
-param = {'out_name': 'fisheries_waste_flux_'}
+param = {'vel_max': 60*100/(3600*6)} # Based on a drifter swinging in a 60m radius
 
 # DIRECTORIES
 dirs = {'script': os.path.dirname(os.path.realpath(__file__)),
@@ -47,8 +47,7 @@ dirs = {'script': os.path.dirname(os.path.realpath(__file__)),
         'fig': os.path.dirname(os.path.realpath(__file__)) + '/../FIGURES/'}
 
 # FILE HANDLES
-fh = {'cmems_grid': dirs['grid'] + 'griddata.nc',
-      'gdp': sorted(glob(dirs['gdp'] + 'bd*.dat')),
+fh = {'gdp': sorted(glob(dirs['gdp'] + 'bd*.dat')),
       'gebco': dirs['grid'] + 'LOC/gebco_2021/GEBCO_2021.nc',
       'coast_005deg': dirs['grid'] + 'LOC/coastal_mask/coastal_mask_005.gpkg',
       'coast_083deg': dirs['grid'] + 'LOC/coastal_mask/GSHHS_h_L1_buffer083_res01.tif',
@@ -58,36 +57,12 @@ fh = {'cmems_grid': dirs['grid'] + 'griddata.nc',
 # LOAD DATA ###################################################################
 ###############################################################################
 
-# Load LSM + coastal cells ####################################################
-with Dataset(fh['cmems_grid'], mode='r') as nc:
-    lon_bnd = nc.variables['lon_rho'][:]
-    lat_bnd = nc.variables['lat_rho'][:]
-    lon_bnd = np.concatenate((lon_bnd, [180]))
-
-    lsm = nc.variables['iso_psi_all'][:]
-    lsm_cst = nc.variables['iso_psi'][:]
-    lsm[lsm > 0] = 1
-    lsm_cst[lsm_cst > 0] = 1
-
 # Load coarse + rasterised 0.083deg coastal mask ##############################
-coast83_obj = gdal.Open(fh['coast_083deg'])
-coast83 = np.flipud(coast83_obj.ReadAsArray()[:])
-
-# Coordinate system
-xdim = coast83_obj.RasterXSize
-ydim = coast83_obj.RasterYSize
-gt = coast83_obj.GetGeoTransform()
-x_lim = [gt[0], gt[0] + xdim*gt[1] + ydim*gt[2]]
-y_lim = [gt[3], gt[3] + xdim*gt[4] + ydim*gt[5]]
-gt_res = gt[1]
-
-lon_c83 = np.linspace(x_lim[0], x_lim[1], num=xdim)
-lat_c83 = np.linspace(y_lim[0], y_lim[1], num=ydim)[::-1]
-
-lon_c83_bnd = np.concatenate((lon_c83, [lon_c83[-1]+gt[1]]))-gt[1]/2
-lat_c83_bnd = np.concatenate((lat_c83, [lat_c83[-1]+gt[1]]))-gt[1]/2
-
 coast83 = xr.open_rasterio(fh['coast_083deg'])
+bnd83_dx = np.mean(np.gradient(coast83.x))
+bnd83_dy = np.mean(np.gradient(coast83.y))
+coast83_bnd_lon = np.concatenate((coast83.x, [coast83.x.values[-1]+bnd83_dx])) - 0.5*bnd83_dx
+coast83_bnd_lat = np.concatenate((coast83.y, [coast83.y.values[-1]+bnd83_dy])) - 0.5*bnd83_dy
 
 # Load 0.005deg (500m) coastal mask ###########################################
 coast5_obj = gpd.read_file(fh['coast_005deg'])
@@ -98,6 +73,22 @@ bath_data = xr.open_dataset(fh['gebco'])
 ###############################################################################
 # ANALYSE GDP TRAJECTORIES ####################################################
 ###############################################################################
+
+# Keep track of some basic statistics
+stats = {'rejected_latitude': 0,  # Rejected due to latitude bounds
+         'rejected_no_coast': 0,  # Rejected due to no coastal intercept
+         'coast_no_beach': 0,     # Valid, no beaching event
+         'coast_beach_prox': 0,   # Valid, beaching event via proximity criterion
+         'coast_beach_depth': 0,  # Valid, beaching event via depth criterion
+         'coast_beach_both': 0,}  # Valid, beaching event via both criteria
+
+coast_time = []   # Cumulative time spent at coast by trajectory (s)
+beach_status = [] # Beaching status: 0 = no beach
+                  #                  1 = proximity beach
+                  #                  2 = depth beach
+                  #                  3 = proximity + depth beach
+
+
 for gdp_fh in fh['gdp']:
     # Read file
     df = pd.read_csv(gdp_fh, sep='\s+', header=None,
@@ -113,58 +104,117 @@ for gdp_fh in fh['gdp']:
     drifter_id_list = np.unique(df['ID'])
 
     # Loop through drifters
-    for drifter_id in drifter_id_list:
+    for drifter_id in tqdm(drifter_id_list, total=len(drifter_id_list)):
         df_drifter = df.loc[df['ID'] == drifter_id].copy()
+        df_drifter = df_drifter.reset_index(0)
 
-        # Carry out an initial sweep to identify if there is any overlap with
-        # coastal cells
-        gridded_drifter_loc = np.histogram2d(df_drifter['lon'].values,
-                                             df_drifter['lat'].values,
-                                             bins=[lon_c83_bnd, lat_c83_bnd])[0].T
-
-        if np.sum(gridded_drifter_loc*coast83):
-            # Assess whether points are within 1/12 degree of coast
-            coast_status = np.zeros((len(df_drifter)))
-
-            for i in tqdm(range(len(df_drifter)), total=len(df_drifter)):
-                lon_idx = np.searchsorted(lon_c83_bnd, df_drifter.iloc[i]['lon'])-1
-                lat_idx = np.searchsorted(lat_c83_bnd, df_drifter.iloc[i]['lat'])-1
-                coast_status[i] = 1 if coast83[lat_idx, lon_idx] == 1 else 0
-
-                # pos = Point(df_drifter.iloc[i]['lon'], df_drifter.iloc[i]['lat'])
-                # within_coast = coast.geometry.intersects(pos)[0]
-                # coast_status = 1 if within_coast else 0
-
-            df_drifter['coast_status'] = coast_status
-
-            # if np.sum(coast_status):
-            #     print()
-
-            f, a0 = plt.subplots(1, 1, figsize=(10, 10),
-                                 subplot_kw={'projection': ccrs.PlateCarree()})
-
-            # a0.pcolormesh(lon_cm_bnd, lat_cm_bnd, coast_cm, cmap=cmr.neutral)
-            a0.scatter(df_drifter['lon'], df_drifter['lat'], s=1)
-            a0.scatter(df_drifter['lon'].loc[df_drifter['coast_status'] == 1],
-                       df_drifter['lat'].loc[df_drifter['coast_status'] == 1],
-                       c=df_drifter['coast_status'].loc[df_drifter['coast_status'] == 1],
-                       vmin=0, vmax=1)
-            a0.coastlines()
-            a0.set_xlim([np.min(df_drifter['lon']-1), np.max(df_drifter['lon'])+1])
-            a0.set_ylim([np.min(df_drifter['lat']-1), np.max(df_drifter['lat']+1)])
-            plt.show()
-            print()
+        # Reject if drifter goes further than 60deg from equator
+        if np.any(np.abs(df_drifter['lat']) > 60):
+            # Reject due to latitude criterion
+            stats['rejected_latitude'] += 1
         else:
-            print('Rejected!')
+            # Assess coastal status of each location
+            df_drifter['coast'] = coast83.interp(coords={'x': xr.DataArray(df_drifter['lon'].values, dims='z'),
+                                                         'y': xr.DataArray(df_drifter['lat'].values, dims='z')},
+                                                 method='nearest').values[0]
 
-        print()
+            # Only continue with trajectories that ever went near the coast
+            if np.sum(df_drifter['coast']) > 0:
+                # Assess whether drifter meets beaching criteria
+                lon_end = df_drifter['lon'].iloc[-1]
+                lat_end = df_drifter['lat'].iloc[-1]
 
+                # Test proximity criterion
+                end_pos = Point(lon_end, lat_end)
+                proximity_beach = coast5_obj.geometry.intersects(end_pos)[0]
 
+                # Test depth criterion
+                end_depth = bath_data.interp(coords={'lon': lon_end,
+                                                     'lat': lat_end},
+                                             method='linear')['elevation'].values
+                depth_beach = True if end_depth > -30 else False
 
+                if proximity_beach or depth_beach:
+                    # If trajectory has beached, try to determine when exactly it
+                    # beached (assume drifter is swinging in a 60m radius)
+                    #
+                    # Start at the penultimate time index and work backwards until
+                    # velocity exceeds threshold
 
+                    tidx = len(df_drifter)-1
+                    beach_found = False
+                    while not beach_found:
+                        tidx -= 1
+                        if df_drifter['vel'].iloc[tidx] > param['vel_max']:
+                            beach_found = True
 
+                    # Now calculate cumulative time by coasts at this time (in days)
+                    drifter_time_at_coast = np.cumsum(df_drifter['coast'])[tidx+1]*0.25
+                else:
+                    drifter_time_at_coast = np.sum(df_drifter['coast'])*0.25
 
+                # Write data
+                if proximity_beach and depth_beach:
+                    stats['coast_beach_both'] += 1
+                    beach_status.append(3)
+                elif proximity_beach:
+                    stats['coast_beach_prox'] += 1
+                    beach_status.append(1)
+                elif depth_beach:
+                    stats['coast_beach_depth'] += 1
+                    beach_status.append(2)
+                else:
+                    stats['coast_no_beach'] += 1
+                    beach_status.append(0)
 
+                coast_time.append(drifter_time_at_coast)
+
+                # if depth_beach:
+                #     f, a0 = plt.subplots(1, 1, figsize=(10, 10),
+                #                          subplot_kw={'projection': ccrs.PlateCarree()})
+
+                #     a0.scatter(df_drifter['lon'], df_drifter['lat'], s=1)
+                #     a0.scatter(df_drifter['lon'].loc[df_drifter['coast'] == 1],
+                #                df_drifter['lat'].loc[df_drifter['coast'] == 1],
+                #                c=df_drifter['coast'].loc[df_drifter['coast'] == 1],
+                #                vmin=0, vmax=1)
+                #     a0.coastlines()
+                #     a0.set_xlim([np.min(df_drifter['lon']-1), np.max(df_drifter['lon'])+1])
+                #     a0.set_ylim([np.min(df_drifter['lat']-1), np.max(df_drifter['lat']+1)])
+                #     plt.show()
+                #     print()
+
+            else:
+                # Reject due to no coastal intercept
+                stats['rejected_no_coast'] += 1
+
+###############################################################################
+# DERIVE BEACHING RATE ########################################################
+###############################################################################
+
+beach_status = np.array(beach_status)
+beach_status[beach_status > 0] = 1
+
+coast_time = np.array(coast_time)
+coast_time_beach = coast_time*beach_status
+
+time_array_bnd = np.linspace(0, 20, num=11)
+time_array =  0.5*(time_array_bnd[1:] + time_array_bnd[:-1]) # x axis for plot
+
+# Now bin
+n_drifters_per_ct_bin = np.histogram(coast_time, bins=time_array_bnd)[0]
+n_unbeached_drifters_per_ct_bin = np.histogram(coast_time, bins=time_array_bnd, weights=1-beach_status)[0]
+
+# Calculate unbeached fraction per bin
+f_unbeached = np.zeros_like(time_array)
+sig_threshold = 5 # Minimum number of drifters per bin for significance
+for i in range(len(time_array)):
+    if n_drifters_per_ct_bin[i] <= sig_threshold:
+        f_unbeached[i] = np.nan
+    else:
+        f_unbeached[i] = n_unbeached_drifters_per_ct_bin[i]/n_drifters_per_ct_bin[i]
+
+plt.scatter(time_array, f_unbeached)
 
 
 
